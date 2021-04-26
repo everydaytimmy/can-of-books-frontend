2863d67b596dbf2d95079c47fb65bc43ae99583a
‘Timothy <Timothy.viccari@gmail.com>
2021-04-26 16:46:18 -0400

    initial commits

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-4rwdhvtd.us.auth0.com"
+    clientId="HWQV98E0OwgjRPfHq6GwZCcmO0SfrZHE"
+    redirectUri="http://localhost:3000"
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 );
